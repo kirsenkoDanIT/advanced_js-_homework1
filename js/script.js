39090d32fffbc6c2c793e5909354cf14bc0ecbe5
kirsenko <kirsenko8000@gmail.com>
2019-06-29 14:33:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -180,15 +180,15 @@ console.log("Price: %f", hamburger.calculatePrice());
 // сколько стоит
 // console.log("Price: %f", hamburger.calculatePrice());
 // я тут передумал и решил добавить еще приправу
-// hamburger.addTopping(Hamburger.TOPPING_SPICE);
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
 // А сколько теперь стоит? 
-// console.log("Price with sauce: %f", hamburger.calculatePrice());
+console.log("Price with sauce: %f", hamburger.calculatePrice());
 // Проверить, большой ли гамбургер? 
-// console.log("Is hamburger large: %s", hamburger.getSize() === Hamburger.SIZE_LARGE); // -> false
-// console.log("This size is %s", hamburger.getSize().name); // -> false
-// console.log("This stuffing is %s", hamburger.getStuffing().name);
+console.log("Is hamburger large: %s", hamburger.getSize() === Hamburger.SIZE_LARGE); // -> false
+console.log("This size is %s", hamburger.getSize().name); // -> false
+console.log("This stuffing is %s", hamburger.getStuffing().name);
 // Убрать добавку
-// hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
 console.log("Have %d toppings", hamburger.getToppings().length); // 1
 
 
